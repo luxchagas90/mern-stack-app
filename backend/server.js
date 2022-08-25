e62726646747aec20ca20df92dbe5efa5f6801b6
Lucas Chagas <96440592+luxchagas90@users.noreply.github.com>
2022-08-25 21:53:31 +0100

    express router and API routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express, { json } from 'express';
 import dotenv from 'dotenv';
 
+import { router as workoutRoutes } from './routes/workouts.js';
+
 // evironment variables
 dotenv.config();
 const port = process.env.PORT;
@@ -9,15 +11,15 @@ const port = process.env.PORT;
 const app = express();
 
 // middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 })
 
 // routes
-app.get('/', (req, res) => {
-    res.json({ mssg: "Welcome to the app" })
-})
+app.use('/api/workouts/', workoutRoutes);
 
 // listen for requests
 app.listen(port, () => {
